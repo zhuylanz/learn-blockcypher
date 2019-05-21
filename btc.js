7639cef6f261c934f4b53c42e365df40c106c231
Lan Nguyen <zhuylanz@gmail.com>
2019-05-21 17:44:41 +0700

    update

--- btc.js
+++ btc.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 
-const BTC_API_URL = "https://api.blockcypher.com/v1/btc/test3";
+const BTC_API_URL = "https://api.blockcypher.com/v1/bcy/test";
 const BTC_API_TOKEN = "296130378220499ab143192bc8211001";
-const createBtcUrl = url => BTC_API_URL + url;
+const createBtcUrl = url => BTC_API_URL + url + "?token=" + BTC_API_TOKEN;
 
 module.exports.createWallet = data =>
   axios.post(createBtcUrl("/wallets"), {
@@ -35,9 +35,7 @@ module.exports.addAddressToWallet = (name, addresses) =>
   });
 
 module.exports.createAddress = () =>
-  axios.post(createBtcUrl("/addrs"), {
-    params: { token: BTC_API_TOKEN }
-  });
+  axios.post(createBtcUrl("/addrs")).then(resp => resp.data);
 
 module.exports.getAddress = address =>
   axios.get(createBtcUrl(createBtcUrl(`/addrs/${address}`)), {
@@ -53,3 +51,36 @@ module.exports.getBalance = address =>
   axios.get(createBtcUrl(`/addrs/${address}/balance`), {
     params: { token: BTC_API_TOKEN }
   });
+
+const convertToSatoshi = (amount, fromUnit) => {
+  return amount;
+};
+
+const genTxObj = (inputAddress, outputAddress, satoshiAmount) =>
+  axios
+    .post(createBtcUrl("/txs/new"), {
+      inputs: [{ addresses: [inputAddress] }],
+      outputs: [{ addresses: [outputAddress], value: satoshiAmount }]
+    })
+    .then(resp => resp.data);
+
+const signTx = txObj => someSigningFunc(txObj, privKey);
+
+const broadcastTx = signedTxObj =>
+  axios.post(createBtcUrl("/txs/send"), signedTxObj).then(resp => resp.data);
+
+module.exports.transferCoin = async (
+  inputAddress,
+  outputAddress,
+  btcAmount,
+  inputAddressPrivKey
+) => {
+  const satoshiAmount = convertToSatoshi(btcAmount);
+  const txObj = await genTxObj(inputAddress, outputAddress, satoshiAmount);
+  console.log("> txObj", txObj);
+
+  const signedTxObj = signTx(txObj);
+  console.log("> signedTxObj", signedTxObj);
+
+  return broadcastTx(signedTxObj);
+};
--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const BTC = require("./btc");
 
 const address1 = {
-	private: "571ed1de0c368eb7ae3e77c9601d5f7dda48189a67e0d3587af61fb2e8be232a",
+	private: "7cf01d537016d6b0731b799da3b2630e37aa0da771b287413b00c7a5ff1df6a6",
 	public:
-		"02f237a3725dd40fd01f16c1cb1cceb8bced1d38a99c1dbd90cf0279a9ff921615",
-	address: "mm9nYMBLzXEjA1xn8YW6Jzi96qugBzJY7K",
-	wif: "cQW3zAp14VoXFb88jbZyWxUhenPUZZrYDp7hgwP4v5QsrF84umhb"
+		"032904ad8657ef8d75fcf2505a3929e65779c88b03f010bf83100dbab1c6bd273e",
+	address: "BxSDUtq7GvJ7XXwaXvQd8xiNexgvZXYUbn",
+	wif: "BsWto4VTnffHRpxwC7AUQUfJufBzxb27ojJUgxXKvf8fB3cCv4H9"
+};
+
+const address2 = {
+	private: "646c7e429a5af2736c804b18f050de61ebe1844920e631d1273925ca7d0b34bc",
+	public:
+		"03044cd1b843f6e0e85edaee3e31afe7dd55fef11f63d00ba329d5deb735c93edc",
+	address: "BubwGwrbExHMqZo1gq5roFvJKfSei4gJiP",
+	wif: "BrhExjfR8dZqoqTj3tFfgZ6qAGQRBqZ5ru8jUq3mFqS8JeqNp7fW"
 };
 
 async function createAddress() {
 	const response = await BTC.createAddress();
-	console.log(response.data);
+	console.log(response);
 }
 
 async function getBalance() {
@@ -18,4 +26,15 @@ async function getBalance() {
 	console.log(response.data);
 }
 
-getBalance();
+async function transferCoin() {
+	await BTC.transferCoin(
+		address1.address,
+		address2.address,
+		10000,
+		address1.private
+	);
+}
+
+// getBalance();
+// createAddress();
+transferCoin().catch(resp => console.log("ERR", resp.response.data));
